374013653d1cd6ad1862bc9c7335a82d98c7aca6
Ashen Adikari <ashen.adikari@arimaclanka.com>
2023-02-24 00:19:19 +0530

    ultrasonic_node

--- catkin_ws/src/sensor_nodes/raspberry_pi/ultrasonic.py
+++ catkin_ws/src/sensor_nodes/raspberry_pi/ultrasonic.py
@@ -1,38 +1,12 @@
-#!/usr/bin/python
-import RPi.GPIO as GPIO
+#!/usr/bin/python3
+# import RPi.GPIO as GPIO
 import time
+from ultrasonic_node import *
 
-try:
-      GPIO.setmode(GPIO.BOARD)
+### ultrasonic pins ###
+trigPin1 = 18
+echoPin1 = 24
+index1 = 1
 
-      PIN_TRIGGER = 18
-      PIN_ECHO = 24
-
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      print "Waiting for sensor to settle"
-
-      time.sleep(2)
-
-      print "Calculating distance"
-
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-      time.sleep(0.00001)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
-
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print "Distance:",distance,"cm"
-
-finally:
-      GPIO.cleanup()
+if __name__ == "__main__":
+        us1 = ultrasonic(trigPin1, echoPin1, index1)
---
+++ catkin_ws/src/sensor_nodes/raspberry_pi/ultrasonic_node.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+import rospy
+from std_msgs.msg import Int16
+
+GPIO.setmode(GPIO.BOARD)
+
+class ultrasonic():
+
+    def __init__(self, trig, echo, index):
+        self.trigPin = trig
+        self.echoPin = echo
+        self.index = index
+
+        self.distance = Int16
+        self.publish_rate = 10
+
+        GPIO.setup(self.trigPin, GPIO.OUT)
+        GPIO.setup(self.echoPin, GPIO.IN)
+
+        ### Publisher ###
+        topic = "ultrasonic_"+str(self.index)+"_dis"
+        self.ultrasonic_pub = rospy.Publisher(topic, Int16, queue_size=10)
+        rospy.init_node("ultrasonic_"+str(self.index))
+        self.rate = rospy.Rate(self.publish_rate)
+
+        GPIO.output(self.trigPin, GPIO.LOW)
+        print("Waiting for sensor to settle")
+        self.distance = 0
+        self.distance_calculate()
+    
+    def distance_calculate(self):
+        while not rospy.is_shutdown():
+
+            GPIO.output(self.trigPin, GPIO.HIGH)
+
+            time.sleep(0.00001)
+
+            GPIO.output(self.trigPin, GPIO.LOW)
+
+            while GPIO.input(self.echoPin)==0:
+                    pulse_start_time = time.time()
+            while GPIO.input(self.echoPin)==1:
+                    pulse_end_time = time.time()
+
+            pulse_duration = pulse_end_time - pulse_start_time
+            self.distance.data = round(pulse_duration * 17150, 2)
+            print("Distance:",self.distance.data,"cm")
+            
+            ### publish the distance ###
+            self.ultrasonic_pub.publish(self.distance)
+
+            self.rate.sleep()
+    
+
+if __name__ == "__main__":
+    us = ultrasonic(18, 24, 1)
